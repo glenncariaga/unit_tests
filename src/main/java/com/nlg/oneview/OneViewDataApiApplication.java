0c7d04f9c37c9e0c3f8a4dda7685e592c2d1cae0
glenncariaga <glenncariaga@gmail.com>
2019-06-11 19:50:55 -0400

    added code -first commit

---
+++ src/main/java/com/nlg/oneview/OneViewDataApiApplication.java
@@ -0,0 +1,13 @@
+package com.nlg.oneview;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OneViewDataApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OneViewDataApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/nlg/oneview/OneViewDataApiApplicationTests.java
@@ -0,0 +1,16 @@
+package com.nlg.oneview;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class OneViewDataApiApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
